e03e46d632e615088b30b225857c6ee5bf1ea071
Marco <m.hebischoutlook.com>
2026-03-30 12:02:03 +0200

    solved js loops challenges

---
+++ js-loops/js-loops_stars/js/index.js
@@ -0,0 +1,23 @@
+console.clear();
+
+const starContainer = document.querySelector('[data-js="star-container"]');
+
+function renderStars(filledStars) {
+  // Reset the star container before re-rendering stars
+  starContainer.innerHTML = "";
+
+  // --v-- write or modify code below this line --v--
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    //console.log(filledStars);
+    star.src = i <= filledStars ? "assets/star-filled.svg" : "assets/star-empty.svg";
+    star.alt = "";
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(star);
+  }
+  // --^-- write or modify code above this line --^--
+}
+
+renderStars();
